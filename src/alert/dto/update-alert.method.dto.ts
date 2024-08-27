41dce650a10312b4d39e9b404e37d7e834df3c0f
hoamx <hoa.mx@kiotviet.com>
2024-08-27 17:05:15 +0700

    fix: dto for swagger definition

--- src/alert/dto/update-alert.method.dto.ts
+++ src/alert/dto/update-alert.method.dto.ts
@@ -1,11 +1,17 @@
 import { AlertMethodTypes } from '@app/common/enums';
 import { IsEnum, IsOptional } from 'class-validator';
 import { AlertMethodDetailsDto } from './alert-method-details.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateAlertMethodDto {
   @IsEnum(AlertMethodTypes)
+  @ApiProperty({
+    description: 'Method type',
+    example: AlertMethodTypes.EMAIL,
+  })
   method_type: AlertMethodTypes;
 
   @IsOptional()
+  @ApiProperty()
   details?: AlertMethodDetailsDto;
 }
